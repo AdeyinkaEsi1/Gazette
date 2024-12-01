9b27c2c516f62330062fae3a0d3259f78c534cd0
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-12-01 13:06:49 +0100

    upd

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -21,9 +22,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const corsOptions = {
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],  // Allow both localhost and 127.0.0.1
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization']  // Allow these headers
+};
+app.use(cors(corsOptions))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
